47b0bd3e1fc5a7b16ea7fbb4d94df162ef0c0e61
Marvin <marvin.zwolsman@gmail.com>
2018-05-28 16:16:18 +0200

    Not logged in, redirect to login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
 import {map, tap} from 'rxjs/operators';
@@ -9,14 +9,19 @@ import {map, tap} from 'rxjs/operators';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
     return this.authService.user$.pipe(
-      tap(user => console.log('user from auth guard', user)),
-      map(user => user !== null)
+      map(user => user !== null),
+      tap(canActivate => {
+        if (canActivate) {
+          return;
+        }
+        this.router.navigate(['/login']);
+      })
     );
   }
 }
