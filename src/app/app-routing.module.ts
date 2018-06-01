05a770d52fdb38d4d8272547f3d3f77da038174b
Marvin <marvin.zwolsman@gmail.com>
2018-06-01 14:48:06 +0200

    More formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './pages/login/login.component';
-import {CarsComponent} from './pages/basic/cars/cars.component';
-import {AuthGuard} from './guards/auth.guard';
-import {LoginGuard} from './guards/login.guard';
-import {TripsComponent} from './pages/basic/trips/trips.component';
-import {InvoicesComponent} from './pages/basic/invoices/invoices.component';
-import {RoleGuard} from './guards/role.guard';
-import {PoliceHomeComponent} from './pages/police/police-home/police-home.component';
-import {AllCarsComponent} from './pages/police/all-cars/all-cars.component';
-import {CarDetailsComponent} from './pages/police/car-details/car-details.component';
-import {ROLE_BASIC, ROLE_GOV, ROLE_POLICE} from './models/roles';
-import {CartrackersListComponent} from './pages/gov/cartrackers-list/cartrackers-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CarsComponent } from './pages/basic/cars/cars.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
+import { TripsComponent } from './pages/basic/trips/trips.component';
+import { InvoicesComponent } from './pages/basic/invoices/invoices.component';
+import { RoleGuard } from './guards/role.guard';
+import { AllCarsComponent } from './pages/police/all-cars/all-cars.component';
+import { CarDetailsComponent } from './pages/police/car-details/car-details.component';
+import { ROLE_BASIC, ROLE_GOV, ROLE_POLICE } from './models/roles';
+import { CartrackersListComponent } from './pages/gov/cartrackers-list/cartrackers-list.component';
 
 const routes: Routes = [
   {
