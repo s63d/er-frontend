09e46c97b5944f66958bc2650dfbd6fb7300c23d
Marvin <marvin.zwolsman@gmail.com>
2018-06-01 10:59:24 +0200

    Redirect a wrong role to the correct home path

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import {User} from '../models/user';
+import {ROLE_BASIC, ROLE_BASIC_HOME, ROLE_GOV, ROLE_GOV_HOME, ROLE_POLICE, ROLE_POLICE_HOME} from '../models/roles';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -18,7 +19,24 @@ export class RoleGuard implements CanActivate {
     const role = next.data.role || '';
 
     return this.auth.user$.pipe(
-      map((user: User) => user.role.toUpperCase() === role.toUpperCase())
+      tap((user: User) => {
+        const userRole = user.role.toUpperCase();
+        if (userRole === role.toUpperCase()) {
+          return;
+        }
+        switch (userRole) {
+          case ROLE_POLICE:
+              this.router.navigateByUrl(ROLE_POLICE_HOME);
+            return;
+          case ROLE_GOV:
+            this.router.navigateByUrl(ROLE_GOV_HOME);
+            return;
+          case ROLE_BASIC:
+            this.router.navigateByUrl(ROLE_BASIC_HOME);
+            return;
+        }
+      }),
+      map((user: User) => user.role.toUpperCase() === role.toUpperCase()),
     );
   }
 }
---
+++ src/app/models/roles.ts
@@ -0,0 +1,7 @@
+export const ROLE_BASIC = 'BASIC';
+export const ROLE_GOV = 'GOVERNMENT';
+export const ROLE_POLICE = 'POLICE';
+
+export const ROLE_BASIC_HOME = '/cars';
+export const ROLE_GOV_HOME = '/gov/cars';
+export const ROLE_POLICE_HOME = '/police/cars';
