633c9cbb264f82bd647392d1681b2d8cb4aa8244
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-11 13:52:03 +0200

    Added invoice detail component

---
+++ src/app/pages/basic/invoices/invoice-detail-user/invoice-detail-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-detail-user',
+  templateUrl: './invoice-detail-user.component.html',
+  styleUrls: ['./invoice-detail-user.component.css']
+})
+export class InvoiceDetailUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
