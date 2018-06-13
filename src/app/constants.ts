1fa5a98f8fb75dbef7700173c1e580551a9b6a03
Marvin <marvin.zwolsman@gmail.com>
2018-06-13 22:34:03 +0200

    Updated protocol to HTTPS

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const PROTOCOL = 'http'
-export const HOST = 'ersols.online'
-export const BASE_URL = `${PROTOCOL}://${HOST}`
-export const API_BASE_URL = `${BASE_URL}/api`
+export const PROTOCOL = 'https';
+export const HOST = 'ersols.online';
+export const BASE_URL = `${PROTOCOL}://${HOST}`;
+export const API_BASE_URL = `${BASE_URL}/api`;
