195712de7822b28d368e528f6d8a0485effc1e43
Marvin <marvin.zwolsman@gmail.com>
2018-06-16 10:57:38 +0200

    Show trip polyline in vehicle color

--- src/app/pages/basic/trips/trips.component.ts
+++ src/app/pages/basic/trips/trips.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {combineLatest, Observable, of} from 'rxjs';
 import {filter, map, tap} from 'rxjs/operators';
 import * as polylineUtil from '@mapbox/polyline';
 import {BasicService} from '../../../services/basic.service';
@@ -15,82 +15,38 @@ import * as _ from 'lodash';
 export class TripsComponent implements OnInit {
 
   trips = {};
-  polylines = [];
-  tripsChanged(vehicleId, trips) {
-    this.trips[vehicleId] = trips;
+  polylines: Observable<any>;
+  tripsChanged(vehicleId, selectedTrips) {
+    this.trips[vehicleId] = selectedTrips;
 
-    const arr = [];
-    const ids = Object.keys(this.trips);
-    for (const id of ids) {
-      for (const trip of this.trips[id]) {
-        if (trip.polyline === '') {
-          continue;
-        }
+    this.polylines = combineLatest(of(this.trips), this.vehicles$).pipe(
+      map(obj => {
+        const vehicles = obj[1];
+        const trips = obj[0];
 
-        const decoded = {'type': 'Feature',
-                          'properties': {},
-                          'geometry': polylineUtil.toGeoJSON(trip.polyline)
-                        };
-        arr.push(decoded);
+        const arr = [];
+        const ids = Object.keys(trips);
+        for (const id of ids) {
+          for (const trip of trips[id]) {
+            if (trip.polyline === '') {
+              continue;
+            }
+
+            const decoded = {
+              'type': 'Feature',
+              'properties': {},
+              'geometry': polylineUtil.toGeoJSON(trip.polyline)
+            };
+            arr.push({
+              color: vehicles.filter(v => v.id === id)[0].color,
+              geo: decoded
+            });
+            // arr.push(decoded);
+          }
       }
-    }
-    this.polylines = arr;
+      return arr;
+      }));
   }
-  // trips$ = of([
-  //   {
-  //     'tripId': 1,
-  //     'length': 3587,
-  //     'polyline': 'kt~cHo~jrA_A_X{@_XgC}UzDyYzKyb@~U{N~ZbAhZgLhK}g@',
-  //     'date': '2018-05-21T12:28:02.000+0000',
-  //     'carTrackerId': '8873028c-3358-42b7-8199-a72901d10e70'
-  //   },
-  //   {
-  //     'tripId': 2,
-  //     'length': 25439,
-  //     'polyline': 'iux{GebixAeAOmB~FKdRrBtRCfRbKbSjLlUhJtUnH|XdNrj@|Hvk@mBbq@gHzm@mEnn@`Adn@ne@bTtPnv@aSln@_[|d@aJtt@zKvt@vNfw@xWfg@tE`w@tF|w@rItx@bIxv@fIrx@hChy@sJvt@_Fbz@iRjn@eIpu@\\pz@jFru@mLfr@Dbs@xUxm@rL|u@_Hnt@aKzz@eJ~u@gPno@',
-  //     'date': '2018-05-21T12:28:26.000+0000',
-  //     'carTrackerId': '8873028c-3358-42b7-8199-a72901d10e70'
-  //   },
-  //   {
-  //     'tripId': 3,
-  //     'length': 4923,
-  //     'polyline': 'uzfeHi~evAeCvHeA`KsEzJyIeDqTgXy^y`Au]c_Aa]i}@sTmhA',
-  //     'date': '2018-05-22T09:17:34.000+0000',
-  //     'carTrackerId': '8873028c-3358-42b7-8199-a72901d10e70'
-  //   }
-  // ]).pipe(
-  //   map(rawTrips => {
-  //     const trips = [];
-  //     for (const trip of rawTrips) {
-  //       let newTrip = {... trip};
-  //       newTrip.polyline = {'type': 'Feature',
-  //                 'properties': {},
-  //                 'geometry': polylineUtil.toGeoJSON(trip.polyline)
-  //               };
-  //       trips.push(newTrip);
-  //     }
-  //     return trips;
-  //   })
-  //);
-
-  selectedTrips = [];
-
-
-  // // TODO make this fancy......
-  // polylines$ = this.trips$.pipe(
-  //   map(trips => {
-  //     const polylines = [];
-  //     for (const trip of trips) {
-  //       if (trip.polyline === '') {
-  //         continue;
-  //       }
-  //       polylines.push({'type': 'Feature',
-  //         'properties': {},
-  //         'geometry': polylineUtil.toGeoJSON(trip.polyline)
-  //       });
-  //     }
-  //     return polylines;
-  //   }));
 
   statuses$: Observable<String[]>;
   vehicles$: Observable<BasicVehicle[]>;
@@ -103,14 +59,12 @@ export class TripsComponent implements OnInit {
     this.statuses$ = this.vehicles$.pipe(
       map(vehicles => _.groupBy(vehicles, 'status')),
       map(groups => Object.keys(groups)),
-      tap(console.log)
     );
   }
 
 
   filteredVehicles(status: String): Observable<BasicVehicle[]> {
     return this.vehicles$.pipe(
-      tap(console.log),
       map(vehicles => vehicles.filter(v => v.status === status))
     );
   }
